2b583e3a298d0c13f3980172d98bc812b5d5b413
januarekaputra <ekaputrajanuar14@gmail.com>
2022-08-09 11:01:30 +0800

    new file test.js and i added eslintrc.json in test.js i use for try extensions

---
+++ test.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+export default class test extends Component {
+  render() {
+    const variabelBaru = 'Nama Saya';
+    color = '#0223c9';
+    return <div>test</div>;
+  }
+}
